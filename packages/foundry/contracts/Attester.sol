b1f875afcf0eefac77dbb9367314d68dfbe2b608
Ethan Rouimi <contact@ethan-rouimi.com>
2023-08-09 20:48:50 +0100

    change Attester contract name

--- packages/foundry/contracts/Attester.sol
+++ packages/foundry/contracts/Attester.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import {IEAS, AttestationRequest, AttestationRequestData} from "@eas/contracts/IEAS.sol";
 import {NO_EXPIRATION_TIME, EMPTY_UID} from "@eas/contracts/Common.sol";
 
-contract AttesterCustom {
+contract Attester {
     // The address of the global EAS contract.
 
     bytes32 private s_attestSkillSchema;
@@ -13,12 +13,12 @@ contract AttesterCustom {
     IEAS private immutable s_eas;
     mapping(address => bool) private s_Authorized;
 
-    error InvalidEAS();
-    error NotAuthorized();
+    error Attester__InvalidEAS();
+    error Attester__NotAuthorized();
 
     modifier onlyAuthorized() {
         if (s_Authorized[msg.sender] != true) {
-            revert NotAuthorized();
+            revert Attester__NotAuthorized();
         }
         _;
     }
@@ -33,52 +33,43 @@ contract AttesterCustom {
 
     constructor(address _eas) {
         if (address(_eas) == address(0)) {
-            revert InvalidEAS();
+            revert Attester__InvalidEAS();
         }
 
         s_eas = IEAS(_eas);
         s_Authorized[msg.sender] = true;
     }
 
-    function endorseSkill(
-        bytes32 _refUID,
-        address _user,
-        bool _endorse
-    ) internal returns (bytes32) {
-        return
-            s_eas.attest(
-                AttestationRequest({
-                    schema: s_endorseSkillSchema, //endore schema
-                    data: AttestationRequestData({
-                        recipient: _user,
-                        expirationTime: NO_EXPIRATION_TIME, // No expiration time
-                        revocable: true,
-                        refUID: _refUID,
-                        data: abi.encode(_endorse),
-                        value: 0 // No value/ETH
-                    })
+    function endorseSkill(bytes32 _refUID, address _user, bool _endorse) internal returns (bytes32) {
+        return s_eas.attest(
+            AttestationRequest({
+                schema: s_endorseSkillSchema, //endore schema
+                data: AttestationRequestData({
+                    recipient: _user,
+                    expirationTime: NO_EXPIRATION_TIME, // No expiration time
+                    revocable: true,
+                    refUID: _refUID,
+                    data: abi.encode(_endorse),
+                    value: 0 // No value/ETH
                 })
-            );
+            })
+        );
     }
 
-    function attestSkill(
-        address _user,
-        string memory _skill
-    ) internal returns (bytes32) {
-        return
-            s_eas.attest(
-                AttestationRequest({
-                    schema: s_attestSkillSchema, // attest skill schema
-                    data: AttestationRequestData({
-                        recipient: _user, // No recipient
-                        expirationTime: NO_EXPIRATION_TIME, // No expiration time
-                        revocable: true,
-                        refUID: EMPTY_UID, // No references UI
-                        data: abi.encode(_skill),
-                        value: 0 // No value/ETH
-                    })
+    function attestSkill(address _user, string memory _skill) internal returns (bytes32) {
+        return s_eas.attest(
+            AttestationRequest({
+                schema: s_attestSkillSchema, // attest skill schema
+                data: AttestationRequestData({
+                    recipient: _user, // No recipient
+                    expirationTime: NO_EXPIRATION_TIME, // No expiration time
+                    revocable: true,
+                    refUID: EMPTY_UID, // No references UI
+                    data: abi.encode(_skill),
+                    value: 0 // No value/ETH
                 })
-            );
+            })
+        );
     }
 
     function isAuthorized(address _user) public returns (bool isAuthorized) {
